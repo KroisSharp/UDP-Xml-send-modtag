de848c4c9996a025c07393296d7c2164a9dc301e
kroissharp <mikx0067@edu.easj.dk>
2018-04-19 17:26:54 +0200

    Add project files.

---
+++ XMLSender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLSender xmlSender = new XMLSender(420);
+            xmlSender.start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XmlUD/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlUDPReceiver xmlUdpReceiver = new XmlUDPReceiver(420);
+            xmlUdpReceiver.start();
+            Console.ReadLine();
+        }
+    }
+}
